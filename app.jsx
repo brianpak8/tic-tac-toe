b1fd11506b3b18c46081bca47e3b148b98647b96
Brian Pak <brianpak27@gmail.com>
2018-01-08 14:52:35 -0800

    installed node and started app.jsx file to render

---
+++ app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      1: {},
+      2: {},
+      3: {},
+      4: {},
+      5: {},
+      6: {},
+      7: {},
+      8: {},
+      9: {}
+    }
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
