6dfc710e8ebab1f3699a7b9da6ac2bf4b7206d24
Brian Pak <brianpak27@gmail.com>
2018-01-08 16:42:20 -0800

    added a is winner function

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
+const prompt = require('prompt');
+
 const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const tracker = {
+  1: '',
+  2: '',
+  3: '',
+  4: '',
+  5: '',
+  6: '',
+  7: '',
+  8: '',
+  9: ''
+}
 
 const winningCombos = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8],
 [3, 6, 9], [3, 5, 7], [4, 5, 6], [7, 8, 9]];
+
+const isWinner = (player) => {
+  for (let i = 0; i < winningCombos.length; i++) {
+    if (tracker[winningCombos[i][0]] === tracker[winningCombos[i][1]] && tracker[winningCombos[i][0]] === tracker[winningCombos[i][2]]) {
+      return true;
+    }
+  }
+  return false;
+}
